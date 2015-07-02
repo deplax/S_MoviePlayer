9119d6d049ef4160823c094dea98e911717a7e60
deplax <sturdyegg@gmail.com>
2015-07-02 22:27:37 +0900

    doodoo

--- src/player/MainStage.java
+++ src/player/MainStage.java
@@ -1,11 +1,35 @@
 package player;
 
+import java.io.File;
+
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.stage.FileChooser;
 
-public class MainStage extends Application{
+public class MainStage extends Application {
+
+	String fileName = null;
+	File file = null;
+
+	TableView<MoviePlayer> table = new TableView<>();
+	ObservableList<MoviePlayer> data = FXCollections.observableArrayList();
+
+	int no = 1;
 
 	public static void main(String[] args) {
 		launch(args);
@@ -14,16 +38,104 @@ public class MainStage extends Application{
 	@Override
 	public void start(javafx.stage.Stage primaryStage) throws Exception {
 
+		Scene scene = new Scene(new Group());
 		primaryStage.setTitle("primaryStage");
-		Group root = new Group();
-		//FlowPane root = new FlowPane();
-		
-		Button btn = new Button("test");
-		btn.setOnAction(eve-> new MoviePlayer());
-		root.getChildren().add(btn);
-		
-		Scene scene = new Scene(root, 400, 400);
+		primaryStage.setWidth(400);
+		primaryStage.setHeight(500);
+
+		Label label = new Label("Image Controller");
+		label.setFont(new Font("Arial", 20));
+		table.setPrefSize(330, 400);
+
+		// Table column Setting
+		TableColumn columnFileName = new TableColumn("FileName");
+		columnFileName
+				.setCellValueFactory(new PropertyValueFactory<MoviePlayer, String>(
+						"fileName"));
+		TableColumn columnStatus = new TableColumn("Status");
+		columnStatus
+				.setCellValueFactory(new PropertyValueFactory<MoviePlayer, String>(
+						"status"));
+		TableColumn columnCurrentTime = new TableColumn("CurrentTime");
+		columnCurrentTime
+				.setCellValueFactory(new PropertyValueFactory<MoviePlayer, String>(
+						"CurrentTime"));
+
+		table.setItems(data);
+		table.getColumns().addAll(columnCurrentTime, columnFileName,
+				columnStatus);
+
+		TextField filePath = new TextField();
+		filePath.setPrefWidth(300);
+		filePath.setDisable(true);
+		filePath.setText("Select movie...");
+
+		Button findFileButton = new Button("...");
+		findFileButton.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				FileChooser fileChooser = new FileChooser();
+				fileChooser.setTitle("Open File");
+				file = fileChooser.showOpenDialog(primaryStage);
+				filePath.setText(file.getAbsolutePath());
+				fileName = file.getName();
+			}
+		});
+
+		Button addButton = new Button("add");
+		addButton.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				if (file != null) {
+					data.add(new MoviePlayer(file, fileName));
+					no++;
+					file = null;
+					filePath.setText("Select movie...");
+				}
+			}
+		});
+
+		Button delButon = new Button("del");
+		delButon.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				MoviePlayer m = table.getSelectionModel().getSelectedItem();
+				if (m != null) {
+					System.out.println(m.getFileName());
+					m.subStage.close();
+					data.remove(m);
+				}
+			}
+		});
+
+		HBox hboxTable = new HBox();
+		hboxTable.setSpacing(5);
+		hboxTable.getChildren().addAll(table, delButon);
+
+		HBox hbox = new HBox();
+		hbox.setSpacing(5);
+		hbox.getChildren().addAll(filePath, findFileButton, addButton);
+
+		VBox vbox = new VBox();
+		vbox.setSpacing(5);
+		vbox.setPadding(new Insets(10, 0, 0, 10));
+		vbox.getChildren().addAll(label, hboxTable, hbox);
+
+		((Group) scene.getRoot()).getChildren().addAll(vbox);
+
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
 }
+
+// 메인 창이 6개의 서브 창을 컨트롤 가능해야 함.
+// 서브 창은 별도의 화면에서 전체화면이 가능해야 함.
+
+// 동영상 파일을 2개 받자.
+// 동영상 플레이어를 만든다.
+// 전체 화면이 되도록 한다.
+
+// 컨트롤러 최대화 안 되도록.
--- src/player/MoviePlayer.java
+++ src/player/MoviePlayer.java
@@ -1,25 +1,26 @@
 
 
 package player;
-//
-//import java.nio.file.Path;
-//import java.nio.file.Paths;
-//
-//import javafx.util.Duration;
-//import javafx.application.Application;
-//import javafx.beans.value.ChangeListener;
-//import javafx.beans.value.ObservableValue;
-//import javafx.event.EventHandler;
-//import javafx.scene.Group;
-//import javafx.scene.Scene;
-//import javafx.scene.control.Slider;
-//import javafx.scene.input.MouseEvent;
-//import javafx.scene.layout.VBox;
-//import javafx.scene.media.Media;
-//import javafx.scene.media.MediaPlayer;
-//import javafx.scene.media.MediaView;
-//import javafx.scene.paint.Color;
-//import javafx.stage.Stage;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import javafx.util.Duration;
+import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Slider;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 //
 //// http://download.eclipse.org/efxclipse/updates-released/1.2.0/site
 //// https://www.youtube.com/watch?v=bWl98dhvf8Q
@@ -99,19 +100,87 @@ package player;
 //	}
 //}
 
+
+import javafx.beans.property.SimpleStringProperty;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
 public class MoviePlayer{
-	public MoviePlayer() {
+
+	private final SimpleStringProperty fileName;
+	private final SimpleStringProperty status;
+	private final SimpleStringProperty currentTime;
+	
+	Stage subStage = new Stage();
+	
+	public MoviePlayer(File file, String fileName) {
 		
-		Stage subStage = new Stage();
+		this.fileName = new SimpleStringProperty(file.getName());
+		this.status = new SimpleStringProperty("stop");
+		this.currentTime = new SimpleStringProperty("currentTime");
+
 		subStage.setTitle("subStage");
-		Group root = new Group();
-		Scene Scene = new Scene(root, 200, 200);
-		subStage.setScene(Scene);
+		Group root = new Group();	
+		
+		Path path = Paths.get(file.getAbsolutePath());
+		Media media = new Media(path.toUri().toString());
+
+		final MediaPlayer player = new MediaPlayer(media);
+		MediaView view = new MediaView(player);
+
+		final VBox vbox = new VBox();
+		//Slider slider = new Slider();
+		//vbox.getChildren().add(slider);
+
+		root.getChildren().add(view);
+		root.getChildren().add(vbox);
+
+		Scene scene = new Scene(root, 400, 400, Color.BLACK);
+		subStage.setScene(scene);
 		subStage.show();
+		
+		player.play();
+		player.setOnReady(new Runnable() {
+
+			@Override
+			public void run() {
+
+				int w = player.getMedia().getWidth();
+				int h = player.getMedia().getHeight();
+
+				System.out.println("w : " + w);
+				System.out.println("h : " + h);
+				subStage.setMinWidth(w);
+				subStage.setMinHeight(h);
+
+				vbox.setMinSize(w - 50, 100);
+				vbox.setTranslateX(25);
+				vbox.setTranslateY(h - 100);
+				
+				view.setFitWidth(subStage.getWidth());
+				view.setFitHeight(subStage.getHeight());
+
+//				slider.setMin(0.0);
+//				slider.setValue(0.0);
+//				slider.setMax(player.getTotalDuration().toSeconds());
+			}
+
+		});
 	}
+
+	public String getFileName() {
+		return fileName.get();
+	}
+
+	public String getStatus() {
+		return status.get();
+	}
+	
+	public String getCurrentTime() {
+		return currentTime.get();
+	}
+
 
 }
