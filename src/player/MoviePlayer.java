e3b34bb48b52b3a630f80eaaf5bfb6b833396505
deplax <sturdyegg@gmail.com>
2015-07-22 17:55:54 +0900

    비율, 크기, 바인딩 문제 모두 해결.

--- src/player/MoviePlayer.java
+++ src/player/MoviePlayer.java
@@ -9,8 +9,6 @@ package player;
 import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Timer;
-import java.util.TimerTask;
 
 import javafx.beans.binding.Bindings;
 import javafx.beans.property.DoubleProperty;
@@ -24,15 +22,13 @@ import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
 import javafx.scene.paint.Color;
-import javafx.stage.Screen;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 import javafx.stage.WindowEvent;
 import javafx.util.Duration;
 
@@ -53,9 +49,9 @@ public class MoviePlayer {
 	double mouseY = 0;
 	double mousediffX = 0;
 	double mousediffY = 0;
-	
+
 	Boolean mouseMoveFlag = true;
-	
+
 	long startTime = System.currentTimeMillis();
 	long endTime = 0;
 
@@ -78,10 +74,21 @@ public class MoviePlayer {
 		this.sound.set(player.isMute() ? "mute" : "unmute");
 
 		final VBox vbox = new VBox();
-
-		root.getChildren().add(view);
+		BorderPane borderPane = new BorderPane();
+		
+		borderPane.setPrefSize(400, 400);
+		borderPane.setCenter(view);
+		
+		vbox.getChildren().addAll(borderPane);
 		root.getChildren().add(vbox);
-
+		
+		final DoubleProperty borderPaneW = borderPane.prefWidthProperty();
+		borderPaneW.bind(Bindings.selectDouble(view.sceneProperty(), "width"));
+		
+		final DoubleProperty borderPaneH = borderPane.prefHeightProperty();
+		borderPaneH.bind(Bindings.selectDouble(view.sceneProperty(), "height"));
+		
+		
 		final DoubleProperty width = view.fitWidthProperty();
 		final DoubleProperty height = view.fitHeightProperty();
 		width.bind(Bindings.selectDouble(view.sceneProperty(), "width"));
@@ -136,16 +143,12 @@ public class MoviePlayer {
 			@Override
 			public void run() {
 
-				int w = player.getMedia().getWidth();
-				int h = player.getMedia().getHeight();
-
-				subStage.setMinWidth(w);
-				subStage.setMinHeight(h);
-				subStage.sizeToScene();
-
-				vbox.setMinSize(w - 50, 100);
-				vbox.setTranslateX(25);
-				vbox.setTranslateY(h - 100);
+				 int w = player.getMedia().getWidth();
+				 int h = player.getMedia().getHeight();
+				
+				 subStage.setMinWidth(w);
+				 subStage.setMinHeight(h);
+				 subStage.sizeToScene();
 
 				// windows7 frame size set
 				subStage.setWidth(subStage.getWidth()
@@ -182,27 +185,28 @@ public class MoviePlayer {
 				scene.setCursor(Cursor.DEFAULT);
 			}
 		});
-		
 
 		player.currentTimeProperty().addListener(
 				new ChangeListener<Duration>() {
-					
+
 					@Override
 					public void changed(
 							ObservableValue<? extends Duration> observable,
 							Duration oldValue, Duration newValue) {
 
-						System.out.println(endTime - startTime);
+						// System.out.println(view.get);
+
+						// System.out.println(endTime - startTime);
 
 						// 마우스가 움직이지 않았으면.
 						if (mouseX == mousediffX && mouseY == mousediffY)
 							mouseMoveFlag = false;
 						else
 							mouseMoveFlag = true;
-						
-						if(con.isMouseMove()){
+
+						if (con.isMouseMove()) {
 							startTime = System.currentTimeMillis();
-						}else{
+						} else {
 							endTime = System.currentTimeMillis();
 							if (endTime - startTime > 5000)
 								scene.setCursor(Cursor.NONE);
@@ -210,7 +214,7 @@ public class MoviePlayer {
 
 						mousediffX = mouseX;
 						mousediffY = mouseY;
-						
+
 						updateValue();
 						con.reflashTable();
 					}
